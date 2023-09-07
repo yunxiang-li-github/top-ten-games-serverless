784fdcfdce44a01b422c8040c25a5fb49c20d1e2
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-09-07 00:04:48 -0400

    init commit

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+// This prevents Mongoose from recompiling the model. 
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default User;
